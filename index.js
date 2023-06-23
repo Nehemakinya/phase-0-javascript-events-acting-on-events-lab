3add64c1cccd2a742ccfd8ca360c94bd7c3b74be
Nehema <+>
2023-06-23 12:05:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+let dodger = document.getElementById('dodger')
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace('px', " ")
+    let left = parseInt(leftNumbers, 10)
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+      }   
+  }
+  
+  document.addEventListener('keydown', function(e) {
+    if (e.which === 37) {
+      moveDodgerLeft()
+    }
+  })
+
+  
+  function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumbers, 10);
+
+    if (right < 360) {
+        dodger.style.left = `${right + 1}px`;
+    }
+}
